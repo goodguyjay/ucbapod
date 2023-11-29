7aeb0c005577f1810ce01b5561e5111b87a11e04
jay <125519964+goodguyjay@users.noreply.github.com>
2023-11-28 22:14:29 -0300

    Update script.js
    Adicionei a tradução em um servidor Python remoto do site https://www.pythonanywhere.com

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,42 @@ async function getNerdFactsFromNasa() {
     const text = data.explanation;
     const currentDate = data.date;
 
+    //titulo traduzido
+    const translatedTitle = await translateText(title);
+    const translatedText = await translateText(text);
+
     let titleHtmlElement = document.getElementsByClassName("brand-heading");
     let imgHtmlElement = document.getElementsByClassName("masthead");
     let textHtmlElement = document.getElementsByClassName("main-text");
     let dateHtmlElement = document.getElementsByClassName("current-date");
 
     imgHtmlElement[0].style.backgroundImage = `url(\"${backgroundImg}\")`; // o background
-    titleHtmlElement[0].innerHTML = title; // o título
-    textHtmlElement[0].innerHTML = text; // a explicação
+    titleHtmlElement[0].innerHTML = translatedTitle; // o título
+    textHtmlElement[0].innerHTML = translatedText; // a explicação
     dateHtmlElement[0].innerHTML = currentDate; // atualizando a data atual
 }
+
+//essa função é pra mandar a requisição de tradução do texto pro meu servidor
+async function translateText(text) {
+    // manda o texto pro python em texto
+    // esse fetch é de um servidor python remoto que montei em outro link
+    const response = await fetch("http://goodguyjay.pythonanywhere.com/translate", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: text }),
+    });
+
+    // checar pra ver se não deu nenhum erro na conexão com o servidor
+    if (response.status !== 200) {
+        throw new Error("Error translating text: " + response.status);
+    }
+
+    // receber o texto traduzido da resposta do webserver
+    const translatedText = await response.json();
+    return translatedText.translated_text;
+}
+
 
 getNerdFactsFromNasa();
